5b87f0c8c54cdb48b5786894f2a4ca9c746e5bd3
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-12-07 16:35:14 +0100

    solve day 03

---
+++ 03/Program.fs
@@ -0,0 +1,30 @@
+﻿
+let Priority c =
+    if System.Char.IsUpper c then (int c) - (int 'A') + 27
+                             else (int c) - (int 'a') +  1
+
+[<EntryPoint>]
+let main args =
+    
+    let lines =
+        args.[0]
+        |> System.IO.File.ReadAllLines
+    
+    lines
+    |> Seq.map (Seq.splitInto 2)
+    |> Seq.map (Seq.map Set.ofSeq)
+    |> Seq.map Set.intersectMany
+    |> Seq.map Set.minElement
+    |> Seq.map Priority 
+    |> Seq.sum
+    |> printfn "Part 1: %A"
+
+    lines
+    |> Seq.chunkBySize 3
+    |> Seq.map (Seq.map Set.ofSeq)
+    |> Seq.map Set.intersectMany
+    |> Seq.map Set.minElement
+    |> Seq.map Priority 
+    |> Seq.sum
+    |> printfn "Part 2: %A"
+    0
