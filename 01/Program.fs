fb786807d160ccbc881e2131b0c50d343dceb726
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-12-07 09:28:28 +0100

    solve day 01

---
+++ 01/Program.fs
@@ -0,0 +1,29 @@
+﻿
+let rec SplitBy state key source =
+    match source with
+    | [] -> if state = [] then [] else [state]
+    | x::xs -> if x <> key  then SplitBy (state @ [x]) key xs
+                            else (SplitBy state key []) @ (SplitBy [] key xs)
+
+[<EntryPoint>]
+let main args =
+
+    let calorieList =
+        args.[0]
+        |> System.IO.File.ReadAllLines
+        |> List.ofSeq
+        |> SplitBy [] ""
+        |> List.map (List.map int)
+        |> List.map (List.sum)
+
+    calorieList
+    |> List.max
+    |> printfn "Part 1: %A"
+
+    calorieList
+    |> List.sortDescending
+    |> List.take 3
+    |> List.sum
+    |> printfn "Part 2: %A"
+
+    0
