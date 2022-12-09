71a320c62071e2b5ced22dd738ad9936b0616eb9
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-12-09 12:02:06 +0100

    solve day 06

---
+++ 06/Program.fs
@@ -0,0 +1,17 @@
+﻿
+
+[<EntryPoint>]
+let main args =
+
+    let input = System.IO.File.ReadAllText args.[0]
+
+    let markerPos n = seq {
+        for i in 0 .. input.Length-n do
+            if (input.[i..i+n-1] |> Set.ofSeq |> Set.count) = n then
+                yield i + n
+    }
+
+    printfn "Part 1: %A" (Seq.head (markerPos  4))
+    printfn "Part 2: %A" (Seq.head (markerPos 14))
+
+    0
